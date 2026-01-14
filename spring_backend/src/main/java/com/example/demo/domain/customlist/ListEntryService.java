eeab2d97d96d2069bc7840007f9f9b326da8cce9
Jaradrj <jara.derooij@edu.tbz.ch>
2026-01-14 15:27:28 +0100

    add base structure for service and repository

---
+++ spring_backend/src/main/java/com/example/demo/domain/customlist/ListEntryService.java
@@ -0,0 +1,42 @@
+package com.example.demo.domain.customlist;
+
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
+@Service
+public class ListEntryService {
+
+    private final ListEntryRepository repository;
+
+    public ListEntryService(ListEntryRepository Repository) {
+        this.repository = Repository;
+    }
+
+    public List<ListEntry> getAllEntries() {
+        return repository.findAll();
+    }
+
+    public List<ListEntry> getEntriesByUser(UUID userId) {
+        return repository.findAllByUserId(userId);
+    }
+
+    public void updateEntry(ListEntry oldEntry) {
+        ListEntry updatedEntry = repository
+                .findById(oldEntry.getId())
+                .orElseThrow(() -> new NoSuchElementException("Entry not found"));
+        updatedEntry.setTitle(oldEntry.getTitle());
+        updatedEntry.setText(oldEntry.getText());
+        updatedEntry.setImportance(oldEntry.getImportance());
+        repository.save(updatedEntry);
+    }
+
+    public void deleteEntryById(UUID id) {
+        repository.deleteById(id);
+    }
+
+    public ListEntry saveEntry(ListEntry listEntry) {
+        return repository.save(listEntry);
+    }
+}
