0bd55337e6d947f220249110546a1d22d2162270
Jaradrj <jara.derooij@edu.tbz.ch>
2026-01-14 16:24:23 +0100

    add controller layer for ListEntry

---
+++ spring_backend/src/main/java/com/example/demo/domain/customlist/ListEntryController.java
@@ -0,0 +1,60 @@
+package com.example.demo.domain.customlist;
+
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
+@Validated
+@RestController
+@RequestMapping("/list-entries")
+public class ListEntryController {
+
+    private final ListEntryService entryService;
+
+    @Autowired
+    public ListEntryController(ListEntryService entryService) {
+        this.entryService = entryService;;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ListEntry>> getAllEntries() {
+        List<ListEntry> entries = entryService.getAllEntries();
+        return ResponseEntity.ok(entries);
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<List<ListEntry>> getEntriesByUser(@PathVariable UUID id) {
+        List<ListEntry> entries = entryService.getEntriesByUser(id);
+        return ResponseEntity.ok(entries);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ListEntry> updateEntry(@PathVariable UUID id, @RequestBody @Valid ListEntry entry) {
+        try {
+            entry.setId(id);
+            entryService.updateEntry(entry);
+            return ResponseEntity.ok(entry);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<ListEntry> createEntry(@RequestBody @Valid ListEntry entry) {
+        ListEntry savedEntry = entryService.saveEntry(entry);
+        return new ResponseEntity<>(savedEntry, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ListEntry> deleteEntry(@PathVariable UUID id) {
+        entryService.deleteEntryById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
